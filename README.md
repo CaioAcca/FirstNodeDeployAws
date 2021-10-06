2bc520ae136ebba020210591e9453255d5d471b0
Edson Caio Acca d sILVA <caio.acca@tegra.com.br>
2021-10-05 21:22:51 -0300

    feat: add new endpoint

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # FirstNodeDeployAws
+
+sudo npm install pm2 -g
+
+pm2 list
+
+pm2 start index.js --name=deploy_teste
+
+pm2 stop deploy_teste
--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/', (request, response) => {
     response.json({message: 'Server is Up'});
 });
 
+app.get('/atualizou', (request, response) => {
+    response.json({message: 'Atualizou mesmo'});
+})
+
 app.listen(3333);
